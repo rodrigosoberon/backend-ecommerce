3f0ea0693b6dbcf7fdae3e31a418f83bf2969c0f
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2023-02-20 19:30:22 -0300

    implementar JWT

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,10 +3,13 @@ const { Router } = express
 const router = Router()
 const ProductsController = require('../controllers/products.controller')
 const productsController = new ProductsController()
+const checkAuth = require('../middleware/check-auth')
 
 router.get('/', productsController.getProductsController)
 
 router.get('/:id', productsController.getProductByIdController)
+
+router.use(checkAuth)
 
 router.put('/:id', productsController.putProductController)
 
