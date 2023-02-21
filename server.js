760766f14c2facb12bd9560a7d17e0d031d847b5
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2023-02-20 21:04:19 -0300

    setup de HBS

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express')
 const products = require('./routes/products.routes')
+const hbs = require('express-handlebars')
+
 const initServer = () => {
 	const app = express()
 
-	app.get('/', (request, response) => {
-		response.send('<h1>API running!</h1>')
-	})
 	app.use(express.json())
 	app.use(express.urlencoded({ extended: true }))
 	app.use('/api/products', products)
 
+	app.get('/', (req, res) => {
+		res.render('index', { funciona: 'Si' })
+	})
+
+	//* -------------------- Handlebars setup -------------------- *//
+	app.engine(
+		'hbs',
+		hbs.engine({
+			extname: '.hbs',
+			partialDir: __dirname + '/views/partials',
+			layoutDir: __dirname + '/views/layouts',
+			defaultLayout: 'layout.hbs'
+		})
+	)
+	app.set('views', './views')
+	app.set('view engine', 'hbs')
+	//* -------------------- Handlebars setup -------------------- *//
+
 	app.use((error, req, res, next) => {
 		if (res.headerSent) {
-			//Si ya existe una respuesta
-			return next(error) //sigue la ejecución y pasa el error
-		} //si no existe, creo el error
-		res.status(error.code || 500) //Si no existe codigo de error, asigna 500 por def.
-		res.json({ message: error.message || 'Unknown error ocurred!' }) //Creo mensaje por defecto si no existe
+			return next(error)
+		}
+		res.status(error.code || 500)
+		res.json({ message: error.message || 'Unknown error ocurred!' })
 	})
 
 	return {
